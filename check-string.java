1af7e8b04c8ade0f34854f1d733268b062abe39a
morelandc <159933775+morelandc@users.noreply.github.com>
2024-05-08 14:06:04 -0400

    Update check-string.java

--- check-string.java
+++ check-string.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        String input1 = args[0]
+        String input1 = args[0];
 
         Boolean isExpectedOutput;
 
         if(input1.equals("Hello and welcome!") {
-          isExpectedOutput = true
+          isExpectedOutput = true;
         }else {
-          isExpectedOutput = false
+          isExpectedOutput = false;
         }
+
+        System.out.println(input1);
             
     }
 }
